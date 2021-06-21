022657be5ad7a5672a2a818d562d578f3d01cfd1
Paarth Kothari <65506287+paarth14@users.noreply.github.com>
2021-06-22 00:37:34 +0530

    Create Arrays Q3.js

---
+++ Arrays/Arrays Q3.js
@@ -0,0 +1,29 @@
+//Zero Sum Problem
+
+//Problem Statement - To find the Sum of pair in array whose sum is zero.
+
+//Input :- [-5, -4, -3, -2, 0, 2, 4, 6, 8]
+
+//Output :- [?,?] - Question
+
+
+function getSumPairZero(array)
+{
+	for (let number of array)
+	{
+		for(let j=1; j<array.length; j++)
+		{
+		  if(number + array[j] == 0)
+		  {
+		    return [number,array[j]];
+		  }
+		}
+	}
+}
+
+const result = getSumPairZero([-5, -4, -3, -2, 0, 2, 4, 6, 8]);
+console.log(result);
+
+// Output:
+
+// [ -4, 4 ]
